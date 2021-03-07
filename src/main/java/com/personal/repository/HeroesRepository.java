991cda42764ea2e2f652ba33029bd6921b9708ba
1T Cassio <cassiockt@1T-CASSIO.ccasj.intraer>
2021-03-07 08:38:43 -0300

    Repository with EnableScan annotation

---
+++ src/main/java/com/personal/repository/HeroesRepository.java
@@ -0,0 +1,9 @@
+package com.personal.repository;
+
+import com.personal.document.Heroes;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+@EnableScan
+public interface HeroesRepository extends CrudRepository<Heroes, String>{
+}
